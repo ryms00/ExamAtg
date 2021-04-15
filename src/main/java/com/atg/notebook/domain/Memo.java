b39697f8d2f28e7d04d9f21492728e897d2c99db
ryms00 <ryms00@naver.com>
2021-04-15 14:13:54 +0900

    test

---
+++ src/main/java/com/atg/notebook/domain/Memo.java
@@ -0,0 +1,21 @@
+package com.atg.notebook.domain;
+
+public class Memo {
+	private double sn; 
+    private String content;
+    
+	public double getSn() {
+		return sn;
+	}
+	public void setSn(double sn) {
+		this.sn = sn;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	} 
+    
+    
+}
